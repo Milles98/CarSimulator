447f20772595a4e3c8506df08d679611ee735604
Mille Elfver <mille.elfver98@gmail.com>
2024-05-28 15:33:18 +0200

    `Added AppStartTests class and updated project references`
    ` `
    
    `In this commit, we've added an empty `AppStartTests` class to the `CarSimulatorTests` namespace in `AppStartTests.cs` and removed unnecessary using directives. In `CarSimulatorTests.csproj`, we've added a new package reference to `Moq` version `4.20.70` and included project references to `CarSimulator.csproj` and `Library.csproj`.

--- CarSimulatorTests/AppStartTests.cs
+++ CarSimulatorTests/AppStartTests.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CarSimulatorTests
+﻿namespace CarSimulatorTests
 {
     [TestClass]
     public class AppStartTests
     {
+
     }
 }
