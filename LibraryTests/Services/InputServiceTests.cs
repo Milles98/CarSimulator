31206009f9166768962b295f1020e64eea39c96b
Mille Elfver <mille.elfver98@gmail.com>
2024-05-28 07:28:47 +0200

    `Added new test setup and methods in InputServiceTests`
    ` `
    
    This commit includes the addition of new namespaces `Library.Services`, `Library.Services.Interfaces`, `Microsoft.VisualStudio.TestTools.UnitTesting`, `Moq`, and `System` to support the new test setup and methods. Unnecessary namespaces have been removed.
    
    In the `InputServiceTests` class, a private `Mock<IConsoleService>` object `_consoleServiceMock` and a private `InputService` object `_sut` have been added. A `Setup` method has been added to initialize these objects before each test.
    
    Three new test methods have been added: `GetUserChoice_ShouldReturnValidChoice_WhenInputIsValid`, `GetUserChoice_ShouldReturnMinusOneAndDisplayError_WhenInputIsInvalid`, and `GetUserChoice_ShouldHandleExceptionGracefully`. These tests validate the behavior of the `GetUserChoice` method under different scenarios.

--- LibraryTests/Services/InputServiceTests.cs
+++ LibraryTests/Services/InputServiceTests.cs
@@ -1,13 +1,65 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Library.Services;
+using Library.Services.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System;
 
 namespace LibraryTests.Services
 {
     [TestClass]
     public class InputServiceTests
     {
+        private Mock<IConsoleService> _consoleServiceMock;
+        private InputService _sut;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _consoleServiceMock = new Mock<IConsoleService>();
+            _sut = new InputService(_consoleServiceMock.Object);
+        }
+
+        [TestMethod]
+        public void GetUserChoice_ShouldReturnValidChoice_WhenInputIsValid()
+        {
+            // Arrange
+            _consoleServiceMock.Setup(cs => cs.ReadLine()).Returns("3");
+
+            // Act
+            var result = _sut.GetUserChoice();
+
+            // Assert
+            Assert.AreEqual(3, result);
+        }
+
+        [TestMethod]
+        public void GetUserChoice_ShouldReturnMinusOneAndDisplayError_WhenInputIsInvalid()
+        {
+            // Arrange
+            _consoleServiceMock.Setup(cs => cs.ReadLine()).Returns("invalid");
+
+            // Act
+            var result = _sut.GetUserChoice();
+
+            // Assert
+            Assert.AreEqual(-1, result);
+            _consoleServiceMock.Verify(cs => cs.SetForegroundColor(ConsoleColor.Red), Times.Once);
+            _consoleServiceMock.Verify(cs => cs.WriteLine("Ogiltigt val, försök igen."), Times.Once);
+            _consoleServiceMock.Verify(cs => cs.ResetColor(), Times.Once);
+        }
+
+        [TestMethod]
+        public void GetUserChoice_ShouldHandleExceptionGracefully()
+        {
+            // Arrange
+            _consoleServiceMock.Setup(cs => cs.ReadLine()).Throws(new Exception("Test exception"));
+
+            // Act
+            var result = _sut.GetUserChoice();
+
+            // Assert
+            Assert.AreEqual(-1, result);
+            _consoleServiceMock.Verify(cs => cs.WriteLine(It.Is<string>(s => s.Contains("An error occurred while getting user choice: Test exception"))), Times.Once);
+        }
     }
 }
