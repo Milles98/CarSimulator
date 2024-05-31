1f04c80af5ab27847b2bfe21cdccc09f3570a20f
Mille Elfver <mille.elfver98@gmail.com>
2024-05-31 06:53:10 +0200

    Refactor initialization and dependency management with Autofac
    The application initialization and dependency management have been refactored. The `AppInitializer.cs` and its corresponding test file `AppInitializerTests.cs` have been removed. Initialization is now handled by the Autofac container, which is set up in the new `RegisterAutofac.cs` file. The `AppStart.cs` class has been moved to the `CarSimulator` namespace and modified to use readonly private fields and an asynchronous `AppRun` method. A reference to Autofac version `8.0.0` has been added to `CarSimulator.csproj`. The `AppStartTests.cs` file has been updated to reflect these changes.

---
+++ CarSimulator/Autofac/RegisterAutofac.cs
@@ -0,0 +1,29 @@
+﻿using CarSimulator.Menus.Interface;
+using CarSimulator.Menus;
+using Library.Factory;
+using Library.Services.Interfaces;
+using Library.Services;
+using Autofac;
+
+namespace CarSimulator.Autofac
+{
+    public class RegisterAutofac
+    {
+        public static IContainer RegisteredContainers()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterType<ConsoleService>().As<IConsoleService>().SingleInstance();
+            builder.RegisterType<HttpClient>().SingleInstance();
+            builder.RegisterType<RandomUserService>().As<IRandomUserService>().SingleInstance();
+            builder.RegisterType<MenuDisplayService>().As<IMenuDisplayService>().SingleInstance();
+            builder.RegisterType<SimulationSetupService>().As<ISimulationSetupService>().SingleInstance();
+            builder.RegisterType<InputService>().As<IInputService>().SingleInstance();
+            builder.RegisterType<ActionServiceFactory>().As<IActionServiceFactory>().SingleInstance();
+            builder.RegisterType<MainMenu>().As<IMainMenu>().SingleInstance();
+            builder.RegisterType<AppStart>().SingleInstance();
+
+            return builder.Build();
+        }
+    }
+}
